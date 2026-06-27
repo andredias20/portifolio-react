dcc9e046159f0dc67bcf591bf0bad589628529df
André De Carli Dias <68877082+andredias20@users.noreply.github.com>
2026-06-27 05:08:53 +0000

    chore: add @vercel/speed-insights dependency and integrate SpeedInsights component

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from "@/App";
 import "@/index.css";
 
@@ -11,5 +12,6 @@ createRoot(document.getElementById("root")!).render(
           <App />
       </BrowserRouter>
       <Analytics />
+      <SpeedInsights />
   </StrictMode>
 );
